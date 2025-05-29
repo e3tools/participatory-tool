bb9bf0381c912f6db0358fd590812cfa4b295dfe
Steve Nyaga <stevenyaga@gmail.com>
2025-05-30 01:33:29 +0300

    delete pcra specific files

--- participatory_backend/pcra/doctype/action_phase_template/action_phase_template.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Steve Nyaga and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Action Phase Template", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- participatory_backend/pcra/doctype/strategic_priority_area_other_groups_priority_item/strategic_priority_area_other_groups_priority_item.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, Steve Nyaga and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Strategic Priority Area Other Groups Priority Item", {
-// 	refresh(frm) {
-
-// 	},
-// });
