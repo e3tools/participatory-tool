05032251395e9940762739f8656de51f50bbbf6b
Steve Nyaga <stevenyaga@gmail.com>
2025-05-30 00:35:22 +0300

    feat: Add menu to assign user to engage profiles

--- participatory_backend/engage/doctype/engagement_profile/engagement_profile.js
+++ participatory_backend/engage/doctype/engagement_profile/engagement_profile.js
@@ -1,8 +1,24 @@
 // Copyright (c) 2025, Steve Nyaga and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Engagement Profile", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Engagement Profile", {
+  refresh(frm) {
+    if (!frm.is_new()) {
+      frm.add_custom_button(
+        __("Assign users", [__(frm.doc.name)]),
+        () => {
+          const new_assignment = frappe.model.get_new_doc(
+            "Engagement Profile User Assignment"
+          );
+          frappe.set_route(
+            "Form",
+            "Engagement Profile User Assignment",
+            new_assignment.name,
+            { engagement_profile: frm.doc.name }
+          );
+        },
+        null //__("Assign")
+      );
+    }
+  },
+});
