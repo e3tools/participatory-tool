2de2072f242c65821224aa515305c69ce43d622b
Steve Nyaga <stevenyaga@gmail.com>
2025-06-11 00:27:35 +0300

    feat: Add utility to strip out special characters

--- participatory_backend/utils/common.py
+++ participatory_backend/utils/common.py
@@ -3,22 +3,24 @@ from participatory_backend.enums import TechnicalAnalysisTypeEnum
 from frappe import _
 import re
 
+
 def get_technical_analysis_type(data_type):
     mp = {
-        'Float': TechnicalAnalysisTypeEnum.NUMERIC.value,
-        'Integer': TechnicalAnalysisTypeEnum.NUMERIC.value,
-        'String': TechnicalAnalysisTypeEnum.TEXT.value,
-        'Date': TechnicalAnalysisTypeEnum.DATE.value
+        "Float": TechnicalAnalysisTypeEnum.NUMERIC.value,
+        "Integer": TechnicalAnalysisTypeEnum.NUMERIC.value,
+        "String": TechnicalAnalysisTypeEnum.TEXT.value,
+        "Date": TechnicalAnalysisTypeEnum.DATE.value,
     }
     if data_type in mp:
         return mp[data_type]
     frappe.throw(_(f"Data type {data_type} not mapped to an analysis type"))
 
+
 def get_initials(text: str):
     """
-    Get initials given a string 
+    Get initials given a string
     """
-    res = ''
+    res = ""
     parts = text.strip().replace("  ", " ").split(" ")
     for part in parts:
         if part.isnumeric():
@@ -28,11 +30,24 @@ def get_initials(text: str):
     # return "".join([x[0].upper() for x in text.strip().replace("  ", " ").split(" ")])
     return res
 
+
 def scrub(text: str):
     """
-    Replace special characters then call frappe.scrub 
+    Replace special characters then call frappe.scrub
     """
     if not text:
         return ""
-    txt = '_'.join(re.findall(r'\b\w+\b', text))
+    # txt = '_'.join(re.findall(r'\b\w+\b', text))
+    txt = strip_special_characters(text).strip()
     return frappe.scrub(txt)
+
+
+def strip_special_characters(text: str, strip_numerics=True):
+    if strip_numerics:
+        res = re.sub(r"[^a-zA-Z]", "_", text)  # remove anything that is not text
+    else:
+        res = re.sub(
+            r"[^a-zA-Z0-9]", "_", text
+        )  # remove anything that is not text or number
+    res = re.sub(r"_+", "_", res)  # replace multiple _ with a single one
+    return res.strip()
